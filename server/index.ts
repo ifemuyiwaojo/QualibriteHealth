93b015aeb726adc05a91a4d9d8d9e6926c7e5deb
ifemuyiwaojo <ifeolu.muyiwa-ojo@sjcs.edu>
2026-01-31 08:19:43 -0600

    Change default port to 3150

--- server/index.ts
+++ server/index.ts
@@ -130,7 +130,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const PORT = 5000;
+  const PORT = parseInt(process.env.PORT || "3150", 10);
   server.listen(PORT, "0.0.0.0", () => {
     log(`serving on port ${PORT}`);
   });
